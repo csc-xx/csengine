0000258205b42335b791275b549b0ac97bb89095
Curtis 'cSc' Smith <kman922002@gmail.com>
2008-06-22 21:24:04 -0500

    CSEngine is GPL now

--- kernel.php
+++ kernel.php
@@ -1,4 +1,19 @@
 <?php
+/* This file is part of CSEngine.
+
+CSEngine is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+CSEngine is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with CSEngine.  If not, see <http://www.gnu.org/licenses/>. */
+
 //The csengine kernel. Holds all low-level shit
 //Any assbags who write in here, ADD GORRAM COMMENTS. I want to know how to use it X_X
 // Version tag: 2.0-0
