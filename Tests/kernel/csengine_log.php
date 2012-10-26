c045a83c5094e36a11313415bb854c402e29d884
Curtis C. Smith <kman922002@gmail.com>
2012-10-25 21:23:19 -0500

    Add basic testing for CSEngine's logging subsystem.

--- Tests/kernel/csengine_log.php
+++ Tests/kernel/csengine_log.php
@@ -16,5 +16,15 @@ along with CSEngine.  If not, see <http://www.gnu.org/licenses/>. */
 
 //This is the testfile for the class csengine_log located in kernel.php
 //It will test and output basic debug information pertaining to running CSEngine properly.
-
+require('kernel.php');
+echo "<html>";
+echo "<p> CSEngine Debug : Test/csengine_log.php <hr>";
+echo "<br>Starting new instance of class 'csengine_log'....<br>"; //Output for plaintext.
+echo "<br>Passing arguments: NOTICE, 'This is a test of the csengine_log class'<br>";
+$testlogsubsystem = new csengine_log(NOTICE, "This is a test of the csengine_log class.");
+if ($testlogsubsystem == FALSE) {
+    echo "[FAILED]";
+} else {
+        echo "<hr><br>This concludes the CSEngine test module.</p></html>";
+}
 ?>
