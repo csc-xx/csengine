a2d8dc87285d553cb5793a186e24e37cf405a7c1
Curtis C. Smith <kman922002@gmail.com>
2012-10-25 20:09:48 -0500

    Add GPL template to test files; Commit up-to-date project file for Code::Blocks 10.05

---
+++ Tests/kernel/csengine_log.php
@@ -0,0 +1,20 @@
+<?php
+/* This file is part of CSEngine.
+
+CSEngine is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+CSEngine is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with CSEngine.  If not, see <http://www.gnu.org/licenses/>. */
+
+//This is the testfile for the class csengine_log located in kernel.php
+//It will test and output basic debug information pertaining to running CSEngine properly.
+
+?>
